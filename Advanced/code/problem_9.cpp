535326bf7fb0393d706e1f4312501279ad2e4b27
meenal0205 <95573151+meenal0205@users.noreply.github.com>
2022-10-05 15:38:02 +0530

    Update problem_9.cpp
    updated description of problem 9

--- Advanced/code/problem_9.cpp
+++ Advanced/code/problem_9.cpp
@@ -1,5 +1,11 @@
 
 /*Make a sudoku solver.*/
+
+You are given a suduko your task is to fill the empty boxes with correct number 
+rules of suduko :
+1 each of the digits from  1-9 should appear only once in a row 
+2 each of the digits from  1-9 should appear only once in a column
+3 each of the digits from 1-9 should appear in the 3X3 subgrid 
 // Solved by ShouryaBrahmastra
 #include <iostream>
 #include <vector>
